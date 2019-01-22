c778538706326cab3c0b45fcb2de2679c2bd058f
DELHAY <delhayantoine@gmail.com>
2019-01-22 08:37:50 +0100

    Ajout de l'exercice 1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  document.getElementById("myImg1").style.borderWidth = "10px 10px 10px 10px";
+}
+
+function myFunction2() {
+  document.getElementById("myImg1").style.borderWidth = "0px 0px 0px 0px";
+}
